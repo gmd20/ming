af53af05a24755a03dc401d9dc5c0a06eb19d325
gmd20 <gmd20@163.com>
2017-12-27 17:04:39 +0800

    Update common.c

--- ming/util/common.c
+++ ming/util/common.c
@@ -1,48 +1,52 @@
-
-int format_small_int_padding(char *s, uint32_t i)
-{
-  char *begin = s;
-  if (i >= 1000) {
-    *s++ = '0' + i / 1000;
-    i = i % 1000;
-  }
-
-  *s++ = '0' + i / 100;
-  i = i % 100;
-  *s++ = '0' +  i / 10;
-  i = i % 10;
-  *s++ = '0' + i;
-
-  *s = '\0';
-  return s - begin;
-}
-
+// strftime alternative
 void string_append_timestamp(std::string &s, uint64_t epoch_millisecond)
 {
-  uint64_t last_time;
-  static char timestamp[64]; // "2017-12-25 12:01:02.003"
+  static uint64_t last_time = 0;
+  static char timestamp[32]; // "2017-12-25 12:01:02.003"
 
   uint32_t sec = (uint32_t)(epoch_millisecond / MSEC_PER_SEC);
   uint32_t millisecond = (uint32_t)(epoch_millisecond % MSEC_PER_SEC);
+  int i;
 
-  struct tm * timeinfo;
-  timeinfo = localtime(&sec);
+  if (last_time != sec) {
+    last_time = sec;
+    struct tm * timeinfo;
+    time_t t = (time_t)sec;
+    timeinfo = localtime(&t);
 
-  if (last_time != t) {
-    last_t = t;
-    format_small_int_padding(timestamp, timeinfo->tm_year + 1900);
+    i =  timeinfo->tm_year + 1900;
+    timestamp[0] = '0' + i / 1000;
+    i = i % 1000;
+    timestamp[1] = '0' + i / 100;
+    i = i % 100;
+    timestamp[2] = '0' + i / 10;
+    timestamp[3] = '0' + i % 10;
     timestamp[4] = '-';
-    format_small_int_padding(&timestamp[5], timeinfo->tm_mon + 1);
+    i = timeinfo->tm_mon + 1;
+    timestamp[5] = '0' + i / 10;
+    timestamp[6] = '0' + i % 10;
     timestamp[7] = '-';
-    format_small_int_padding(&timestamp[8], timeinfo->tm_mday);
+    i = timeinfo->tm_mday;
+    timestamp[8] = '0' + i / 10;
+    timestamp[9] = '0' + i % 10;
     timestamp[10] = ' ';
-    format_small_int_padding(&timestamp[11], timeinfo->tm_hour);
+    i = timeinfo->tm_hour;
+    timestamp[11] = '0' + i / 10;
+    timestamp[12] = '0' + i % 10;
     timestamp[13] = ':';
-    format_small_int_padding(&timestamp[14], timeinfo->tm_min);
+    i = timeinfo->tm_min;
+    timestamp[14] = '0' + i / 10;
+    timestamp[15] = '0' + i % 10;
     timestamp[16] = ':';
-    format_small_int_padding(&timestamp[17], timeinfo->tm_sec);
+    i = timeinfo->tm_sec;
+    timestamp[17] = '0' + i / 10;
+    timestamp[18] = '0' + i % 10;
     timestamp[19] = '.';
   }
-  format_small_int_padding(&timestamp[20], millisecond);
+  i = (int) millisecond;
+  timestamp[20] = '0' + i / 100;
+  i = i % 100;
+  timestamp[21] = '0' + i / 10;
+  timestamp[22] = '0' + i % 10;
   s.append(timestamp, 23);
 }
